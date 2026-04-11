47ec6740f505652ec5c2b288f9156bfb3fac4241
modulomedito <chcchc1995@outlook.com>
2026-04-11 18:20:57 +0800

    🎉 init(project):[#]initial commit project

---
+++ rustlike_types.h
@@ -0,0 +1,63 @@
+/// @file rustlike_types.h
+/// @author modulomedito (chcchc1995@outlook.com)
+/// @brief Define rust-like types for C language
+/// @copyright MIT License. Copyright (C) 2026 modulomedito
+/// @details This is a header for defining types like rust
+
+// GUARD START
+#ifndef RUSTLIKE_TYPES_H
+#define RUSTLIKE_TYPES_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// INCLUDE
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+// PUBLIC TYPEDEF
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef float f32;
+typedef double f64;
+typedef uintptr_t usize;
+typedef intptr_t isize;
+typedef char char8;
+typedef char* str;
+
+// PUBLIC DEFINE
+#ifndef bool
+#define bool bool_t
+#endif
+
+#ifndef true
+#define true (1)
+#endif
+
+#ifndef false
+#define false (0)
+#endif
+
+// PUBLIC ENUM
+
+// PUBLIC STRUCT
+
+// PUBLIC UNION
+
+// PUBLIC VARIABLE DECLARATION
+
+// PUBLIC FUNCTION DECLARATION
+
+// GUARD END
+#ifdef __cplusplus
+}
+#endif
+#endif // #ifndef RUSTLIKE_TYPES_H
