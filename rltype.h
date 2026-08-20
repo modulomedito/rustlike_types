a89a7afd306afb7b357d9923626e74b8c2d1cf9b
modulomedito <chcchc1995@outlook.com>
2026-08-21 00:33:41 +0800

    🌈 style(braces):[#]allman style

--- rltype.h
+++ rltype.h
@@ -13,7 +13,8 @@
 #ifndef RLTYPE_H
 #define RLTYPE_H
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 //==============================================================================
@@ -53,6 +54,8 @@ typedef unsigned char uchar;
 #define RLTYPE_MINOR_VERSION (4U)
 #define RLTYPE_PATCH_VERSION (0U)
 
+// Attention: `buf` must not be a pointer, therefore, not apply to the array
+// argument of a function (which actually is a pointer)
 #ifndef RLTYPE_SIZE_OF_BUF
 #define RLTYPE_SIZE_OF_BUF(buf) sizeof((buf)) / sizeof((buf)[0])
 #endif
@@ -85,35 +88,43 @@ typedef unsigned char uchar;
 //==============================================================================
 // PUBLIC INLINE FUNCTION DEFINITION
 //==============================================================================
-static inline void rltype_assert(bool cond) {
+static inline void rltype_assert(bool cond)
+{
 #if defined(__linux__) || defined(__APPLE__) || defined(_WIN32) // Host
     assert(cond);
 #elif defined(__HIGHTEC__) // HighTec (GCC-based, TriCore/Aurix)
 #define util_std_assert(cond)
-    if ((cond) == false) {
+    if ((cond) == false)
+    {
         __builtin_trap();
     }
 #elif defined(__TASKING__) // TASKING
-    if ((cond) == false) {
+    if ((cond) == false)
+    {
         __debug();
     }
 #elif defined(__TI_COMPILER_VERSION__) // TI C2000
-    if ((cond) == false) {
+    if ((cond) == false)
+    {
         asm(" ESTOP0");
     }
 #elif defined(__GNUC__) // Generic GCC (ARM, RISC-V, etc.)
-    if ((cond) == false) {
+    if ((cond) == false)
+    {
         __builtin_trap();
     }
 #else // Unknown platform: infinite loop
-    if ((cond) == false) {
-        for (;;) {
+    if ((cond) == false)
+    {
+        for (;;)
+        {
         }
     }
 #endif
 }
 
-static inline void rltype_panic(void) {
+static inline void rltype_panic(void)
+{
 #if defined(__linux__) || defined(__APPLE__) || defined(_WIN32) // Host
     assert(0);
 #elif defined(__HIGHTEC__) // HighTec (GCC-based, TriCore/Aurix)
@@ -125,7 +136,8 @@ static inline void rltype_panic(void) {
 #elif defined(__GNUC__) // Generic GCC (ARM, RISC-V, etc.)
     __builtin_trap();
 #else // Unknown platform: infinite loop
-    for (;;) {
+    for (;;)
+    {
     }
 #endif
 }
